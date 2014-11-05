9fc5b589620545d7d17c3d5183bd8b528e41979c
Rasmus Eneman <rasmus@eneman.eu>
2014-11-05 17:34:03 +0100

    feat: Scrape course data

--- lib/scraper.dart
+++ lib/scraper.dart
@@ -5,8 +5,65 @@ import 'src/crawler.dart';
 
 const ROOT = 'http://coursepress.lnu.se';
 
+final DATE_PATTERN = new RegExp(r'(\d{4})\-(\d\d)\-(\d\d) (\d\d)\:(\d\d)');
+final SYLLABUS_PATTERN = new RegExp(r'(kursinfo\.lnu\.se/(utbildning/)?GenerateDocument\.ashx)|kursplan.lnu.se');
+
+class Course {
+    String name;
+    String code;
+    String url;
+    String syllabusUrl;
+    String description;
+    Post latestPost;
+}
+
+class Post {
+    String heading;
+    String author;
+    DateTime time;
+}
+
 scrape() {
-    crawl('$ROOT/kurser', _nextPages, 're222dv').listen((p) => print('Got ${p.url}'));
+    crawl('$ROOT/kurser', _nextPages, 're222dv')
+        .where((page) => page.url.startsWith('$ROOT/kurs/'))
+        .listen((PageInfo<Document> page) {
+            var course = new Course()
+                ..name = page.data.querySelector('h1').text.trim()
+                ..code = page.data.querySelector('#header-wrapper ul li:last-child').text.trim()
+                ..url = page.url
+                ..syllabusUrl = page.data.querySelectorAll('a[href]')
+                    .map((a) => a.attributes['href'])
+                    .firstWhere(
+                        (href) => SYLLABUS_PATTERN.hasMatch(href),
+                        orElse: () => null
+                    )
+                ..description = page.data.querySelector('.entry-content>p').text;
+
+            print(course.name);
+            print(course.code);
+            print(course.url);
+            print(course.syllabusUrl);
+            print(course.description);
+
+            var latestPost = page.data.querySelector('#latest-post').parent.nextElementSibling;
+            if (latestPost != null) {
+                var dateMatch = DATE_PATTERN.firstMatch(latestPost.querySelector('.entry-byline').text);
+                course.latestPost = (
+                    new Post()
+                        ..heading = latestPost.querySelector('h1.entry-title').text
+                        ..author = latestPost.querySelector('.entry-byline strong').text
+                        ..time = (
+                            new DateTime.utc(
+                                int.parse(dateMatch.group(1)),
+                                int.parse(dateMatch.group(2)),
+                                int.parse(dateMatch.group(3)),
+                                int.parse(dateMatch.group(4)),
+                                int.parse(dateMatch.group(5))
+                            )
+                        )
+                );
+            }
+        });
 }
 
 Iterable<String> _nextPages(PageInfo<Document> document) {
