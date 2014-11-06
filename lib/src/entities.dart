9955a53846282b5ee6b30a35f962c10e35eac92d
Rasmus Eneman <rasmus@eneman.eu>
2014-11-06 13:35:52 +0100

    refactor: Make entities immutable

--- lib/src/entities.dart
+++ lib/src/entities.dart
@@ -1,16 +1,31 @@
 part of coursepress_scarper;
 
 class Course {
-    String name;
-    String code;
-    String url;
-    String syllabusUrl;
-    String description;
-    Post latestPost;
+    final String name;
+    final String code;
+    final String url;
+    final String syllabusUrl;
+    final String description;
+    final Post latestPost;
+
+    Course({
+        this.name: null,
+        this.code: null,
+        this.url: null,
+        this.syllabusUrl: null,
+        this.description: null,
+        this.latestPost: null
+    });
 }
 
 class Post {
-    String heading;
-    String author;
-    DateTime time;
+    final String heading;
+    final String author;
+    final DateTime time;
+
+    Post({
+        this.heading : null,
+        this.author : null,
+        this.time : null
+    });
 }
--- lib/src/scraper.dart
+++ lib/src/scraper.dart
@@ -5,36 +5,11 @@ const ROOT = 'http://coursepress.lnu.se';
 final DATE_PATTERN = new RegExp(r'(\d{4})\-(\d\d)\-(\d\d) (\d\d)\:(\d\d)');
 final SYLLABUS_PATTERN = new RegExp(r'(kursinfo\.lnu\.se/(utbildning/)?GenerateDocument\.ashx)|kursplan.lnu.se');
 
-/**
- * Scrapes CoursePress on Linnéaus University and returns a [Stream] of [Course]s
- */
+/// Scrapes CoursePress on Linnéaus University and returns a [Stream] of [Course]s
 Stream<Course> scrape() =>
     crawl('$ROOT/kurser', nextPages, 're222dv')
         .where((page) => page.url.startsWith('$ROOT/kurs/'))
-        .map((PageInfo<Document> coursePage) {
-            var document = coursePage.data;
-            var course = new Course()
-                ..name = document.querySelector('h1').text.trim()
-                ..code = document.querySelector('#header-wrapper ul li:last-child').text.trim()
-                ..url = coursePage.url
-                ..description = document.querySelector('.entry-content>p').text
-                ..syllabusUrl = document.querySelectorAll('a[href]')
-                    .map((a) => a.attributes['href'])
-                    .firstWhere(
-                        (href) => SYLLABUS_PATTERN.hasMatch(href),
-                        orElse: () => null
-                    );
-
-            var latestPost = document.querySelector('#latest-post').parent.nextElementSibling;
-            if (latestPost != null) {
-                course.latestPost = new Post()
-                    ..heading = latestPost.querySelector('h1.entry-title').text
-                    ..author = latestPost.querySelector('.entry-byline strong').text
-                    ..time = parseDate(latestPost.querySelector('.entry-byline').text);
-            }
-
-            return course;
-        });
+        .map(parseCourse);
 
 /// Get the next page on course listing if exists and also all courses in the list itself
 Iterable<String> nextPages(PageInfo<Document> document) {
@@ -51,6 +26,34 @@ Iterable<String> nextPages(PageInfo<Document> document) {
     nextPages.addAll(courseLinks);
 
     return nextPages;
+}
+
+/// Parses course data from a course page
+Course parseCourse(PageInfo<Document> coursePage) =>
+    new Course(
+        name: coursePage.data.querySelector('h1').text.trim(),
+        code: coursePage.data.querySelector('#header-wrapper ul li:last-child').text.trim(),
+        url: coursePage.url,
+        description: coursePage.data.querySelector('.entry-content>p').text,
+        syllabusUrl: coursePage.data.querySelectorAll('a[href]')
+            .map((a) => a.attributes['href'])
+            .firstWhere(
+                    (href) => SYLLABUS_PATTERN.hasMatch(href),
+                orElse: () => null
+            ),
+        latestPost: parseLatestPost(coursePage.data)
+    );
+
+/// Parses the latest post on a course page
+Post parseLatestPost(Document coursePage) {
+    var latestPost = coursePage.querySelector('#latest-post').parent.nextElementSibling;
+    if (latestPost == null) return null;
+
+    return new Post(
+        heading: latestPost.querySelector('h1.entry-title').text,
+        author: latestPost.querySelector('.entry-byline strong').text,
+        time: parseDate(latestPost.querySelector('.entry-byline').text)
+    );
 }
 
 /// Returns the first found [DateTime] in [text]
