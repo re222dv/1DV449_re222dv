4a5ec75beb948bdfc4bd7b592d8a14584ba36557
Rasmus Eneman <rasmus@eneman.eu>
2014-12-02 15:52:59 +0100

    feat: List traffic areas

--- Laboration3/server/index.js
+++ Laboration3/server/index.js
@@ -1,9 +1,10 @@
-var Deferred = require("promised-io/promise").Deferred;
+var promise = require("promised-io/promise");
 var fs = require("promised-io/fs");
 var Hapi = require("hapi");
 var request = require('request');
 
 const FIVE_MINUTES = 1000 * 60 * 5;
+const SIX_MONTHS = 1000 * 60 * 60 * 24 * 30 * 6;
 
 var server = new Hapi.Server(9099, { cors: true });
 
@@ -12,15 +13,16 @@ server.route({
     method: 'GET',
     handler: function(_, reply) {
         getData().then(function (data) {
-            reply({messages: data});
+            reply(data);
         }, function(err) {
             throw err;
         });
     }
 });
 
-function cacheData(data) {
-    return fs.writeFile('cache', JSON.stringify({data: data, timestamp: Date.now()})).then(function () {
+function cacheData(areas, messages) {
+    var data = {areas: areas, messages: messages, timestamp: Date.now()};
+    return fs.writeFile('cache', JSON.stringify(data)).then(function () {
         return data;
     }, function() {
         console.log('caching failed');
@@ -32,27 +34,66 @@ function getData() {
     return fs.readFile('cache').then(function(content) {
         var cachedData = JSON.parse(content);
 
-        if (Date.now() - cachedData.timestamp < FIVE_MINUTES) {
-            return cachedData.data;
-        }
+        return promise.all([
+            getAreas(cachedData.timestamp),
+            getTrafficMessages(cachedData.timestamp),
+        ]).then(function (data) {
+            if (data[0] == null) data[0] = cachedData.areas;
+            if (data[1] == null) data[1] = cachedData.messages;
 
-        return getTrafficMessages(cachedData.timestamp).then(function (data) {
-            if (data == null) data = cachedData.data;
-
-            return cacheData(data);
+            return cacheData(data[0], data[1]);
         });
     }, function () {
-        return getTrafficMessages().then(function (data) {
-            return cacheData(data);
+        return promise.all([
+            getAreas(),
+            getTrafficMessages(),
+        ]).then(function (data) {
+            return cacheData(data[0], data[1]);
         });
     });
 }
 
+function getAreas(timestamp) {
+    if (Date.now() - timestamp < SIX_MONTHS) {
+        var deferred = new promise.Deferred();
+        deferred.resolve(null);
+        return deferred.promise;
+    }
+
+    return getRequest('http://api.sr.se/api/v2/traffic/areas?format=json', timestamp).then(function (data) {
+        return data.areas;
+    });
+}
+
 function getTrafficMessages(timestamp) {
-    var deferred = new Deferred();
+    if (Date.now() - timestamp < FIVE_MINUTES) {
+        var deferred = new promise.Deferred();
+        deferred.resolve(null);
+        return deferred.promise;
+    }
+
+    return getRequest('http://api.sr.se/api/v2/traffic/messages?size=1000&format=json', timestamp).then(function (data) {
+        if (data) {
+            data = data.messages
+                .map(function(message) {
+                    message.createddate = new Date(+message.createddate.slice(6, -7)).getTime();
+                    return message;
+                })
+                .sort(function(a, b) {
+                    return b.createddate - a.createddate;
+                })
+                .slice(0, 100);
+        }
+
+        return data;
+    });
+}
+
+function getRequest(url, timestamp) {
+    var deferred = new promise.Deferred();
 
     var options = {
-        uri: 'http://api.sr.se/api/v2/traffic/messages?size=1000&format=json',
+        uri: url,
         json: true,
     };
 
@@ -62,18 +103,7 @@ function getTrafficMessages(timestamp) {
 
     request(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            var data = body.messages;
-            data = data
-                .map(function(message) {
-                    message.createddate = new Date(+message.createddate.slice(6, -7)).getTime();
-                    return message;
-                })
-                .sort(function(a, b) {
-                    return b.createddate - a.createddate;
-                })
-                .slice(0, 100);
-
-            deferred.resolve(data);
+            deferred.resolve(body);
         } else if (!error && response.statusCode == 304) {
             deferred.resolve(null);
         } else {
